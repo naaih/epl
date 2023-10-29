ef0dbfc01a598af3c86aed43589fe8ef2dd93022
naaih <a.naaih@outlook.com>
2023-10-29 21:48:33 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,11 @@ use Illuminate\Support\Facades\Route;
 // Controller
 use App\Http\Controllers\StudentController;
 
+
+// Home
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Records
 Route::get('records',[StudentController::class,'showData']);
