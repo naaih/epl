42e5f302d9aef8aa961496c7c5c2f1b88a9db335
naaih <a.naaih.ahmed@gmail.com>
2023-11-07 08:55:05 +0500

    Added about information to home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container text-center">
+<div class="container text-center mt-5">
   <h1>Student Records</h1>
+  <p>
+    View Student information and Modules information
+  </p>
 </div>
 @endsection
